f12aa7040d18f191ff6dacac3ed85ff910797cf8
ayaz ahmed <ayez_ahmed@hotmail.com>
2020-10-13 19:24:24 +0500

    basic setup

---
+++ src/components/Asteriods.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h1>asteriods</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'asteriods'
+}
+</script>
+
+
+<style lang="scss">
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Asteriods from '@/components/Asteriods.vue';
+import SearchAsteriods from '@/components/SearchAsteriods.vue';
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/asteriods'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/asteriods',
+    component: Asteriods
+  },
+  {
+    path: '/search',
+    component: SearchAsteriods
+  },
+  {
+    path: '*',
+    redirect: '/asteriods'
   }
 ]
 

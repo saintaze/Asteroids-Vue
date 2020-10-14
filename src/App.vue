88d9d90e58649e8fa631cc7cf18d8dd67fc6d349
ayaz ahmed <ayez_ahmed@hotmail.com>
2020-10-14 07:21:46 +0500

    completes basic asteroids component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,58 @@
 <template>
-  <div id="app">
-    <div id="nav">
+  <div class="app">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div> -->
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800&display=swap');
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 62.5%;
+
+  @media (max-width: 1260px) {
+    font-size: 60%;
+  }
+  @media (max-width: 1100px) {
+    font-size: 58%;
+  }
+  @media (max-width: 950px) {
+    font-size: 56%;
+  }
+  @media (max-width: 700px) {
+    font-size: 55%;
+  }
+  @media (max-width: 500px) {
+    font-size: 54%;
+  }
+  @media (max-width: 380px) {
+    font-size: 50%;
+  }
+  @media (max-width: 320px) {
+    font-size: 45%;
+  }
+}
+
+body {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 1.6rem;
+  line-height: 1.6;
   color: #2c3e50;
+  font-weight: normal;
+}
+
+
+.app {
 }
 
 #nav {
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+const API_KEY = 'PTBVCqGiwFzHBdtPO2duHxJ1qlQRUwUndA2KwJ7b';
+export const API_ENDPOINT_ASTEROIDS = `https://api.nasa.gov/neo/rest/v1/neo/browse?page=0&size=12&api_key=${API_KEY}`;
+// export const  = 'Event App';
